b1b3e3976b6c7bee5bd817aa119803faf5cc14ce
pedroacamargo <pedroaugennes@gmail.com>
2023-04-02 11:51:50 +0100

    others repo

--- frontend/scripts/other.js
+++ frontend/scripts/other.js
@@ -1,8 +1,22 @@
+let others = false;
+
   document.getElementById('toggleButton').addEventListener('click', function () {
     const answers1 = document.getElementById('answers1');
     const answers2 = document.getElementById('answers2');
+    const state = document.getElementById("state")
 
-    answers1.classList.toggle('hidden');
-    answers2.classList.toggle('hidden');
+
+    console.log("testee")
+    if (others == false) {
+      answers1.classList.add("hidden");
+      answers2.classList.remove("hidden");
+      state.classList.remove("hidden")
+      others = true
+    } else {
+      answers1.classList.remove("hidden");
+      answers2.classList.add("hidden");
+      state.classList.add("hidden")
+      others = false
+    }
   });
 
